cbae6346318b97778cedc9276e2cc3a91cdd36a8
Susan Ashmore <scashmorecitcom@gmail.com>
2021-06-12 15:08:23 -0400

    add restaurant form working

--- client/src/components/Restaurants/AddRestaurants/AddRestaurants.jsx
+++ client/src/components/Restaurants/AddRestaurants/AddRestaurants.jsx
@@ -1,35 +1,23 @@
 import React, {useState} from 'react';
 import { Button, Modal } from 'react-bootstrap';
+import RestuarantForm from './RestuarantForm/RestaurantForm';
 
-import './style.css'
+
 
 export const AddRestaurants = () => {
     const [show, setShow] = useState(false);
-    const [name, setName] = useState('');
-    const [menuItems, setMenuItems] = useState([]);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    const handleSubmit = () => {
-
-    }
     return (
         <div>
             <Button variant="outline-info" size="lg" onClick={handleShow}>Add Restaurant</Button>
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Create Restuarant</Modal.Title>
+                    <Modal.Title>Create Restaurant</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <form className="restForm" onSubmit={handleSubmit}>
-                        <label>Restuarant Name:</label>
-                        <input type="text"></input>
-                        <label>Cuisine Type:</label>
-                        <input type="text"></input>
-                        <label>Number of Menu Itmes:</label>
-                        <input type="number" min='1' max='15'></input>
-                        <Button className='formBtn' type='submit' variant="primary" onClick={handleClose}>Add This Restuarant</Button>
-                    </form>
+                        <RestuarantForm handleClose={handleClose}/>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>Close</Button>
---
+++ client/src/components/Restaurants/AddRestaurants/RestuarantForm/RestaurantForm.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import './style.css';
+
+
+class RestuarantForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            fields: {"name": "", "cuisine": "", "menu": ""},
+            errors: {}
+        }
+    }
+
+    handleValidation() {
+        let fields = this.state.fields;
+        let errors = {};
+        let formIsValid = true;
+
+        //Name
+
+        if (!fields["name"]) {
+            formIsValid = false;
+            errors["name"] = "Cannot be empty";
+        }
+
+        //Cuisine
+
+        if (!fields["cuisine"]) {
+            formIsValid = false;
+            errors["cuisine"] = "Cannot be empty";
+        }
+
+        if (typeof fields["cuisine"] !== "undefined") {
+            if (!fields["cuisine"].match(/^[a-zA-Z]+$/)) {
+                formIsValid = false;
+                errors["cuisine"] = "Only letters";
+            }
+        }
+
+        //Menu Item #
+        if (!fields["menu"]) {
+            formIsValid = false;
+            errors["menu"] = "Cannot be empty";
+        }
+
+        this.setState({ errors: errors });
+        return formIsValid;
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        if (this.handleValidation()) {
+            this.props.handleClose();
+        } else {
+            alert("Form has errors.")
+        }
+
+    }
+
+    handleChange(field, e) {
+        let fields = this.state.fields;
+        fields[field] = e.target.value;
+        this.setState({ fields });
+    }
+
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit.bind(this)}>
+                <fieldset className="restForm">
+                    <label>Restaurant Name:</label>
+                    <input ref="name" type="text" onChange={this.handleChange.bind(this, "name")} value={this.state.fields["name"]}></input>
+                    <label>Cuisine Type:</label>
+                    <input ref="cuisine" type="text" onChange={this.handleChange.bind(this, "cuisine")} value={this.state.fields["cuisine"]}></input>
+                    <label>Number of Menu Itmes:</label>
+                    <input ref="menu" type="number" min='1' max='15'onChange={this.handleChange.bind(this, "menu")} value={this.state.fields["menu"]}></input>
+                    <input className="formBtn" type="submit" value="Submit"/>
+                </fieldset>
+            </form>
+        );
+    }
+}
+
+export default RestuarantForm
