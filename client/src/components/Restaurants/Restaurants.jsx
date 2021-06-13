c8d876d35826f95562fb04b6b09717ce6e758f8f
Susan Ashmore <scashmorecitcom@gmail.com>
2021-06-13 06:11:34 -0400

    Rest delete based on id now

--- client/src/components/Restaurants/Restaurants.jsx
+++ client/src/components/Restaurants/Restaurants.jsx
@@ -26,21 +26,26 @@ const Restaurants = () => {
                 restName: name.charAt(0).toUpperCase() + name.slice(1),
                 restCuisine: cuisine.charAt(0).toUpperCase() + cuisine.slice(1),
                 restMenu: newMenu,
-                restMenuNum: parseInt(menu)
+                restMenuNum: parseInt(menu),
+                restId: id
                 
             }));
 
     }
 
-    const deleteRestaurant = (restName) => {
-        const newList = restaurants.filter((restaurants) => restaurants.restName !== restName);
+    const updateRestaurant = (restId) => {
+
+    };
+
+    const deleteRestaurant = (restId) => {
+        const newList = restaurants.filter((restaurants) => restaurants.restId !== restId);
         setRestaurants(newList);
-    }
+    };
 
     const deleteMenuItem = (item) => {
         const newList = menuItems.filter((menuItems) => menuItems.item !== item);
         setMenuItems(newList);
-    }
+    };
 
 
     return (
@@ -52,7 +57,7 @@ const Restaurants = () => {
                         <Card.Title className="title">
                             <div>{`${restaurants.restName}`}</div>
                             <EditRestaurant generateRestaurant={generateRestaurant} restName={restaurants.restName} restCuisine={restaurants.restCuisine} restMenu={restaurants.restMenu} restMenuNum={restaurants.restMenuNum} />
-                            <Button variant="link" onClick={() => deleteRestaurant(restaurants.restName)}>❌</Button>
+                            <Button variant="link" onClick={() => deleteRestaurant(restaurants.restId)}>❌</Button>
                         </Card.Title>
                         <Card.Text>
                             {`${restaurants.restName} serves ${restaurants.restCuisine} cuisine and has ${restaurants.restMenuNum} menu items.`}
