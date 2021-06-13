f5e0c545a72d59dfb4309f529b11895129bb5703
Susan Ashmore <scashmorecitcom@gmail.com>
2021-06-12 20:48:52 -0400

    addMenu funciton

--- client/src/components/Restaurants/Menu/Menu.jsx
+++ client/src/components/Restaurants/Menu/Menu.jsx
@@ -6,13 +6,14 @@ const Menu = (restMenu) => {
     const [show, setShow] = useState(false);
     const [menuItems, setMenuItems] = useState(restMenu);
     let menu = [];
-    
 
-    for(var i=0; i<menuItems; i++) {
-    menu = menu.push({item: 'item', description: 'about item'})
-    console.log(menu);
-    };
-   
+    const addMenu = () => {
+        for (var i = 0; i < menuItems; i++) {
+            menu = menu.push({ item: 'item', description: 'about item' })
+        };
+        console.log(menu);
+    }
+
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
